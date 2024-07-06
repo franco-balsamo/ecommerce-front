d697549e7133aef450bdb1bef8c6bda75d321214
franco-balsamo <balsamote96@gmail.com>
2024-07-06 11:24:27 -0300

    Fix to pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/product/:id" component={ProductPage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
